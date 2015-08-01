f2d0cdf4e09671a71f004f75ea460560e42348e1
jwbay <justin.bay@gmail.com>
2015-08-01 02:45:49 -0400

    don't need this in source

--- typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="should/should.d.ts" />
-/// <reference path="through2/through2.d.ts" />
-/// <reference path="vinyl/vinyl.d.ts" />
-/// <reference path="gulp-replace/gulp-replace.d.ts" />
